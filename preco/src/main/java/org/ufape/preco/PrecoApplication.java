14fc3c0134fd7e3a0fa9df614d3ce3a46a7563f0
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2024-08-28 22:18:28 +0000

    Small fixes

--- preco/src/main/java/org/ufape/preco/PrecoApplication.java
+++ preco/src/main/java/org/ufape/preco/PrecoApplication.java
@@ -3,7 +3,6 @@ package org.ufape.preco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class PrecoApplication {
 
